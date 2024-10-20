892ce7cc63f12b4de76843e5762779a64ab2a237
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2024-10-20 04:28:50 +0000

    File Appeal Fixed

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, FileAppeal, AppealSuccess
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("file_appeal/", FileAppeal.as_view(), name="file_appeal"),
+    path('appeal-success/', AppealSuccess.as_view(), name='appeal_success'),
 ]
