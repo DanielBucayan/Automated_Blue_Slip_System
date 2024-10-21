5140baf760493dc8f08f9bd2b8a7fef9de07a85f
Daniel <122674836+DanielBucayan@users.noreply.github.com>
2024-10-21 06:15:23 +0000

    Added proper access control for the admin home page

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,9 @@ from django.utils.timezone import now
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import TemplateView
+from django.contrib import messages
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
@@ -10,9 +13,17 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
 
-
-class AdminHomePageView(TemplateView):
+class AdminHomePageView(UserPassesTestMixin, TemplateView):
     template_name = "pages/admin_home.html"
+
+    # Test to check if the user is an admin
+    def test_func(self):
+        return self.request.user.is_superuser  # or use self.request.user.is_superuser for superadmins
+
+    # Redirect non-admin users
+    def handle_no_permission(self):
+        messages.error(self.request, "You are not authorized to access this page.")
+        return redirect('home')  # Redirect to a 'home' or any other page
 
 # File Appeal View
 @method_decorator(login_required, name='dispatch')  # Ensures the user is logged in
